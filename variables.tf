0aea1935187a1cb686b3e15156baeb217b92e99f
waqas.yousaf <waqas.yousaf@flaconi.de>
2022-06-15 15:40:32 +0200

    OPS-5057: added var type

--- variables.tf
+++ variables.tf
@@ -2,8 +2,12 @@ variable "domain" {
   description = "Cloudflare domain to apply rules for."
   type        = string
 }
-
 variable "page_rules" {
-  description = "Cloudflare page rule configuration list"
-  default     = []
+  description = "List of Cloudflare page rule objects."
+  type = list(object({
+    target      = string
+    status      = bool
+    action      = list(string)
+  }))
+  default = []
 }
