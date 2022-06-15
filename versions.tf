dfd1e40308f6b16324c436685682aebbe531b56f
waqas.yousaf <waqas.yousaf@flaconi.de>
2022-06-15 16:40:51 +0200

    OPS-5057: updated provider

--- versions.tf
+++ versions.tf
@@ -1,3 +1,10 @@
+# https://github.com/cloudflare/terraform-provider-cloudflare
 terraform {
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 3.9.1"
+    }
+  }
   required_version = "~> 1.0"
 }
