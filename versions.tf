39fea49fd33330431aaa57c9637d97054b3ae32b
waqas.yousaf <waqas.yousaf@flaconi.de>
2022-06-21 14:54:31 +0200

    OPS-0: quick fix

--- versions.tf
+++ versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "~> 3.17.0"
+      version = "~> 3.17"
     }
   }
   required_version = "~> 1.0"
